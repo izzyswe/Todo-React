064fc77e0fbba5e0f7f29ba37d68406fae04dd02
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-03-11 14:45:51 -0400

    added styling

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //todos is a variable and settodos is a function creation
+  //use states are to temporarily hold data
+  const [todos, setTodos] = useState(["wash clothes", "do dishes", "watch Friends"])
+  const [userInput, setUserInput] = useState("")
+  //e stands for event as in event listener
+  //target is going to target the tag you specific in this case it is targetting the input tag
+  //value is the input value user has input in this context.
+  const inputHandler = (e) => {
+    setUserInput(e.target.value)
+    console.log(e.target)
+  }
+  const AddInput = (e) =>{
+    e.preventDefault()
+    //... is spreading as in spread all the array out
+    if(userInput === ""){
+      alert("stop, it is a null value")
+    }
+    else{
+      setTodos([...todos, userInput])
+    }
+    setUserInput("")
+  }
   return (
-    <>
+    <div className="App">
+      <h1 className='logo'>Todo App</h1>
+      <form type="submit" onSubmit={AddInput}>
+      <input value={userInput} onChange={inputHandler} type="text"/>
+      <button>Add to do list</button>
+      <p>{userInput}</p>
+      </form>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {
+            //mapping is overlapping the existing array keeping the old one as well as adding new ones
+            todos.map((todo) => {
+              return(
+                <li>{todo}</li>
+              )
+            })
+          }
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
